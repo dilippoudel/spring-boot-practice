73e1e984d16051541b65de0d08b4360b86dd8719
Dilip Poudel <dilippoudel578@gmail.com>
2021-02-01 05:56:50 +0200

    power of ModelAndView in spring boot

--- MyFirstWebApp/src/main/java/com/example/demo/HomeController.java
+++ MyFirstWebApp/src/main/java/com/example/demo/HomeController.java
@@ -6,16 +6,17 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping("home")
-	public String home(HttpServletRequest req, HttpServletResponse res) { 
-		HttpSession session = req.getSession();
-		
-		String name = req.getParameter("name");
-		System.out.println("hi "+ name);
-		session.setAttribute("name", name);
+	public String home(@RequestParam("name")String myName) { 
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name",myName);
+		mv.setViewName("home");
+		System.out.println("hi "+ myName);
 		return "home";
 	}
 }
