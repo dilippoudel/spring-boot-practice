d19a2583ad4914f6ad22b5e3e630b8bbe381503b
Dilip Poudel <dilippoudel578@gmail.com>
2021-02-03 17:13:12 +0200

    Power of Spring Rest Data

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Dilip', 'Java');
+insert into alien values (102, 'Banana', 'Java');
+insert into alien values (105, 'Shailendra', 'Java');
+insert into alien values (103, 'Amber', 'DevOps');
+insert into alien values (104, 'Shiva', 'Nursing');
