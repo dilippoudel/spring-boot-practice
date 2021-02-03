c9a1f01510c1b9fd129f45f5b32516d7de047ab0
Dilip Poudel <dilippoudel578@gmail.com>
2021-02-03 15:19:02 +0200

    CRUD Operation implemented

--- spring-boot-app/src/main/java/com/example/demo/controller/AlienController.java
+++ spring-boot-app/src/main/java/com/example/demo/controller/AlienController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,8 @@ public class AlienController {
 		return "home.jsp";
 	}
 
-	@PostMapping(path="/alien",consumes= {"application/json"}) // now when we post the data, the server will only accepts the json format
+	@PostMapping(path = "/alien", consumes = { "application/json" }) // now when we post the data, the server will only
+																		// accepts the json format
 	public Alien addAlien(@RequestBody Alien alien) { // @RequestBody is to accept the data as request body
 		repo.save(alien);
 		return alien;
@@ -34,9 +37,23 @@ public class AlienController {
 		return repo.findAll();
 	}
 
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		return "Deleted Successfully";
+	}
+
+	@PutMapping(path = "/alien", consumes = { "application/json" })
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+
 	@GetMapping("/alien/{aid}")
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 
 		return repo.findById(aid);
 	}
+
 }
